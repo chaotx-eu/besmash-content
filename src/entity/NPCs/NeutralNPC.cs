eacd2b710788f1d2510dce4eea0f5d77e6eb8459
Larsihasi98 <44375174+Larsihasi98@users.noreply.github.com>
2018-12-16 16:46:14 +0100

    gave neutral NPC a constructor, to avoid errors

--- src/entity/NPCs/NeutralNPC.cs
+++ src/entity/NPCs/NeutralNPC.cs
@@ -2,8 +2,9 @@ namespace BesmashContent
 {
     public class NeutralNPC : NPC
     {
-        public NeutralNPC(BattleManager manager, Stats stats, Ability[] abilities, FightingStyle style) : base(manager, stats, abilities, stats, abilities)
+        public NeutralNPC(BattleManager manager, Stats stats, Ability[] abilities, FightingStyle style) : base(manager, stats, abilities, style)
         {
+            //todo
         }
     }
 }
