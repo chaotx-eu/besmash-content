d859a37686ef7afa157068146b94284c3e64ab56
Larsihasi98 <44375174+Larsihasi98@users.noreply.github.com>
2018-12-16 16:45:05 +0100

    minor bugfixes

--- src/entity/NPCs/NeutralNPC.cs
+++ src/entity/NPCs/NeutralNPC.cs
@@ -2,6 +2,8 @@ namespace BesmashContent
 {
     public class NeutralNPC : NPC
     {
-
+        public NeutralNPC(BattleManager manager, Stats stats, Ability[] abilities, FightingStyle style) : base(manager, stats, abilities, stats, abilities)
+        {
+        }
     }
 }
