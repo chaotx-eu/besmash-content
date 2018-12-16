d8456dfa55263f050159a654971c3ec26cde4bf2
Larsihasi98 <44375174+Larsihasi98@users.noreply.github.com>
2018-12-16 15:39:21 +0100

    Entity comitted und so

--- src/entity/Entity.cs
+++ src/entity/Entity.cs
@@ -1,85 +1,8 @@
 namespace BesmashContent 
 {
     public enum Facing {NORTH, EAST, SOUTH, WEST}
-<<<<<<< HEAD
-    public class Entity : MapObject
-    {
-        public int MaxHP {get{return (BaseStats.VIT + StatsModifier.VIT) * 5;}}
-        public Facing Facing = Facing.SOUTH;
-        public int CurrentHP {get{return CurrentHP;} 
-            set
-            {
-                CurrentHP = value;
-                if(CurrentHP<=0 && !this.status.immortal)
-                    status.dead = true;
-                else if(CurrentHP > MaxHP)
-                    CurrentHP = MaxHP;
-            }}
-        public Stats BaseStats {get; set;}
-        public Stats StatsModifier {get; set;}
-        public Ability[] abilities{get; set;}
-        public Status status{get; set;}
-
-        public BattleManager battleManager{get; set;} //Wird übergeben damit die Entitys nen überblick über das Schlachtfeld haben
-
-        public Entity()
-        {
-            
-        }
-        public Entity(BattleManager manager)
-        {
-            battleManager = manager;
-        }
-
-        public virtual void nextTurn()
-        {
-            //Do nothing by default
-            
-        }
-
-        public bool isDealtDamage(int damage)
-        {
-            if (this.status.invincible)
-                return false;
-            
-            this.onDamage();
-            CurrentHP -= damage;
-            return true;
-        }
-        
-        //Bestimmte Events die überschrieben werde können um Effekte durch bestimmte taten auszulösen.
-        public virtual void onDamage() //Entity hat schaden erhalten
-        {
-            /*
-            Aufwachen, triggern von abilities etc.
-            Will mir hier nur die Möglichkeit offen halten was einzubauen.
-             */
-            if(this.status.asleep)
-            {
-                if (battleManager.random.Next(100) <= (10 * status.roundsAsleep) + 35)
-                {
-                    status.asleep = false;
-                    status.roundsAsleep = 0;
-                }
-            }
-        }
-        public virtual void onHit() //Entity greift erfolgreich einen Gegner an
-        {
-            //Kann überschrieben werden.
-        }
-        public virtual void onKill() //Entity tötet erfolgreich einen Gegner
-        {
-            //Kann überschrieben werden.
-        }
-
-        public virtual void onDeath() //Entity ist gestorben
-        {
-            battleManager.removeFromBattle(this);
-        }
-=======
     public abstract class Entity : MapObject 
     {
         public Facing Facing = Facing.SOUTH;
->>>>>>> 7f7d219bfd78823cd6aaa1a5d6d6ff492e2c2343
     }
 }
