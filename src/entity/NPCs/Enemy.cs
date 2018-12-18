fdbd29bcd161adf1d1e05ce83680c5fa84e7e31b
Larsihasi98 <44375174+Larsihasi98@users.noreply.github.com>
2018-12-18 22:03:41 +0100

    added generic constructor for enemy

--- src/entity/NPCs/Enemy.cs
+++ src/entity/NPCs/Enemy.cs
@@ -3,7 +3,10 @@ namespace BesmashContent
     public class Enemy : NPC
     {
         public int XP{get; set;}
-        public Enemy() {} // TODO quick hotfix for content serializer
+        public Enemy() : base()
+        {
+            XP = 100;
+        }
         public Enemy(Stats stats, Ability[] abilities, FightingStyle style, int xp) : base(stats, abilities, style)
         {
             XP = xp;
